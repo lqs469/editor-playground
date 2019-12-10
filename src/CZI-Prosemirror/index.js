1c05d8b0decfc1f64ec96f780f7a3abb2ff529ea
勤硕 <qinshuo.lqs@alibaba-inc.com>
2019-12-10 10:10:20 +0800

    CZI-Prosemirror https

--- src/CZI-Prosemirror/index.js
+++ src/CZI-Prosemirror/index.js
@@ -28,6 +28,6 @@ import React from 'react';
 export default () => (
   <div style={{ height: "100%" }}>
     <strong>这个库还在开发中，不建议使用，但插件可以参考</strong>
-    <iframe title="czi" src="http://cdn.summitlearning.org/assets/czi_prosemirror_0_0_1_b_index.html" width="100%" height="100%"></iframe>
+    <iframe title="czi" src="https://cdn.summitlearning.org/assets/czi_prosemirror_0_0_1_b_index.html" width="100%" height="100%"></iframe>
   </div>
 )
